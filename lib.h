fe0c7d6063a58c835b5c85c3997ce7dda3a4689e
lysenko_v <lysenko_v@itstep.academy>
2023-11-14 19:45:34 +0200

    1

--- lib.h
+++ lib.h
@@ -1,15 +1,1 @@
-#ifndef LIB_H
-#define LIB_H
-
-class MathOperations {
-public:
-    static int findMin(int x, int y);
-    static int findMax(int x, int y);
-    static int square(int x);
-    static double power(int x, int n);
-    static bool isEven(int x);
-    static bool isOdd(int x);
-};
-
-#endif  
 
